eae9eec29d4801e7345052319add7c9732af45e9
vladimirlukyanov <vladimir.lukyanoff@gmail.com>
2014-09-13 06:32:18 +0400

    v0.0.1
    init commint

---
+++ admin/boilerplate-admin.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Plugin Name: Admin Boilerplate
+ *
+ * @package   boilerplate
+ * @author    NullLogic <hello@nulllogic.net>
+ * @license   MIT
+ * @link      http://www.nulllab.net/development/adminboilderplate
+ * @copyright 2014 NullLogic
+ */
+
+class boilerplate_admin {
+
+	protected static $instance = null;
+
+	protected $plugin_screen_hook_suffix = null;
+
+	private function __construct() {
+
+
+		// Get pub instance of plugin
+		$plugin = boilderplate::get_instance();
+		$this->plugin_slug = $plugin->get_plugin_slug();
+
+//		// Add the options page and menu item.
+//		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
+
+		$this->plugin_screen_hook_suffix = 'appearance_page_'.$this->plugin_slug;
+
+//		// Add an action link pointing to the options page.
+		$plugin_basename = plugin_basename( plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . $this->plugin_slug . '.php' );
+		add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
+
+
+		$this->register_actions();
+
+	}
+
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	public function enqueue_admin_styles() {
+
+		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
+			return;
+		}
+
+		$screen = get_current_screen();
+
+		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
+			wp_enqueue_style( $this->plugin_slug . '-admin-styles', plugins_url( 'assets/css/sf-admin.css', __FILE__ ), array('dashicons'), Plugin_Name::VERSION );
+		}
+
+	}
+
+	/**
+	 * Register and enqueue admin-specific JavaScript.
+	 *
+	 * @TODO:
+	 *
+	 * - Rename "Plugin_Name" to the name your plugin
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    null    Return early if no settings page is registered.
+	 */
+	public function enqueue_admin_scripts() {
+
+		if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
+			return;
+		}
+
+		$screen = get_current_screen();
+		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
+			wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery' ), Plugin_Name::VERSION );
+		}
+
+	}
+
+	/**
+	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_plugin_admin_menu() {
+
+		/*
+		 * Add a settings page for this plugin to the Settings menu.
+		 *
+		 * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+		 *
+		 *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+		 *
+		 * @TODO:
+		 *
+		 * - Change 'Page Title' to the title of your plugin admin page
+		 * - Change 'Menu Text' to the text for menu item for the plugin settings page
+		 * - Change 'manage_options' to the capability you see fit
+		 *   For reference: http://codex.wordpress.org/Roles_and_Capabilities
+		 */
+		$this->plugin_screen_hook_suffix = add_options_page(
+			__( 'Page Title', $this->plugin_slug ),
+			__( 'Menu Text', $this->plugin_slug ),
+			'manage_options',
+			$this->plugin_slug,
+			array( $this, 'display_plugin_admin_page' )
+		);
+
+	}
+
+	/**
+	 * Render the settings page for this plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function display_plugin_admin_page() {
+		include_once( 'views/admin.php' );
+	}
+
+	/**
+	 * Add settings action link to the plugins page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_action_links( $links ) {
+
+		return array_merge(
+			array(
+				'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', $this->plugin_slug ) . '</a>'
+			),
+			$links
+		);
+
+	}
+
+	private function register_actions() {
+
+		// Load admin style sheet and JavaScript.
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+
+
+		add_action( 'admin_menu', array( $this, 'add_to_admin_theme_submenu' ) );
+
+	}
+
+	public function register_filters() {
+
+	}
+
+	public function add_to_admin_theme_submenu() {
+		$page = add_theme_page( 'Favicon settings', 'Favicon', 'manage_options', 'boilerplate', array( $this, 'boilerplate_admin_page' ) );
+	}
+
+	public function boilerplate_admin_page() {
+		include_once( 'views/boilerplate-admin-view.php' );
+	}
+
+}
---
+++ boilderplate.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name:       Admin Boilerplate
+ * Plugin URI:        http://www.nulllab.net/development/adminboilerplate
+ * Description:       Manage favicon icons for your Wordpress website easily and with pleasure
+ * Version:           0.0.1
+ * Author:            @nulllogic
+ * Author URI:        http://www.nulllogic.net/
+ * Text Domain:       adminboilerplate
+ * License:           MIT
+ * License URI:       http://opensource.org/licenses/MIT
+ * Domain Path:       /i18n/languages
+ */
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+// If you want to use this plugin inside you theme and not to list this in Wordpress plugin listing.
+if( ! defined ('USE_PLUGIN_INSIDE_THEME')) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'public/boilerplate-pub.php' );
+
+	register_activation_hook( __FILE__, array( 'boilerplate', 'activate' ) );
+	register_deactivation_hook( __FILE__, array( 'boilerplate', 'deactivate' ) );
+
+	add_action( 'plugins_loaded', array( 'boilerplate', 'get_instance' ) );
+
+	if ( is_admin() ) {
+
+		require_once( plugin_dir_path( __FILE__ ) . 'admin/boilerplate-admin.php' );
+		add_action( 'plugins_loaded', array( 'boilerplate_admin', 'get_instance' ) );
+
+	}
+
+} else {
+
+
+
+}
+
