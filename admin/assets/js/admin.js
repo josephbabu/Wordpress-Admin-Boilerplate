ee12766e80227b5f4b4a5fc45e6594f428e66459
vladimirlukyanov <vladimir.lukyanoff@gmail.com>
2014-09-15 13:18:47 +0400

    add metabox js native support

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -3,7 +3,13 @@
 
 	$(function () {
 
-		// Place your administration-specific JavaScript here
+		/**
+		 * This will be needed for metaboxes in admin page – to drag them and re-order.
+		 */
+
+		jQuery('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+
+		postboxes.add_postbox_toggles('appearance_page_simplefavicon');
 
 	});
 
